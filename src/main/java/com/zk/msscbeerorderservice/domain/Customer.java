32e7ae0d954a0ebb310b5666c66771ea6b86f2a8
ZaeemHasan <syedzaeem25@gmail.com>
2020-11-16 22:44:54 +0100

    - mysql-init.sql added to setup initial DB with a specific user - MySQL properties added in application properties for profile active profile "localmysql" - Column definition for id of Beer entity updated

--- src/main/java/com/zk/msscbeerorderservice/domain/Customer.java
+++ src/main/java/com/zk/msscbeerorderservice/domain/Customer.java
@@ -29,7 +29,7 @@ public class Customer extends BaseEntity {
 
     private String customerName;
 
-    @Column(length = 36, columnDefinition = "varchar")
+    @Column(length = 36, columnDefinition = "varchar(36)")
     private UUID apiKey;
 
     @OneToMany(mappedBy = "customer")
