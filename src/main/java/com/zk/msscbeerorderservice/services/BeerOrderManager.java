9118ecd88203252de769e3dbd1db44eeaf46dd15
ZaeemHasan <syedzaeem25@gmail.com>
2020-11-24 01:16:40 +0100

    Added Initial Beer Order Manager. Closes #8.

---
+++ src/main/java/com/zk/msscbeerorderservice/services/BeerOrderManager.java
@@ -0,0 +1,9 @@
+package com.zk.msscbeerorderservice.services;
+
+import com.zk.msscbeerorderservice.domain.BeerOrder;
+
+public interface BeerOrderManager {
+
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+}
