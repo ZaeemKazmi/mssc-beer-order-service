b61b479718f88049412f3f0386295c86637e7ef1
ZaeemHasan <syedzaeem25@gmail.com>
2020-11-24 22:39:36 +0100

    Handled Validation Order Response. Closes #13.

--- src/main/java/com/zk/msscbeerorderservice/services/BeerOrderManager.java
+++ src/main/java/com/zk/msscbeerorderservice/services/BeerOrderManager.java
@@ -2,8 +2,11 @@ package com.zk.msscbeerorderservice.services;
 
 import com.zk.msscbeerorderservice.domain.BeerOrder;
 
+import java.util.UUID;
+
 public interface BeerOrderManager {
 
     BeerOrder newBeerOrder(BeerOrder beerOrder);
 
+    void processValidationResult(UUID beerOrderId, Boolean isValid);
 }
