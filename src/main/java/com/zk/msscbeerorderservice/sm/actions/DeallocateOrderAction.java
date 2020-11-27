cfbd41eadb18ee9d8c8c38a22e89699b0a1a4a8e
ZaeemHasan <syedzaeem25@gmail.com>
2020-11-27 16:48:59 +0100

    Added Deallocate Order Action which will be triggered on receiving Cancel Order Request when the current state is Allocated. Closes #24.

---
+++ src/main/java/com/zk/msscbeerorderservice/sm/actions/DeallocateOrderAction.java
@@ -0,0 +1,43 @@
+package com.zk.msscbeerorderservice.sm.actions;
+
+import com.zk.brewery.model.events.DeallocateOrderResult;
+import com.zk.msscbeerorderservice.config.JmsConfig;
+import com.zk.msscbeerorderservice.domain.BeerOrder;
+import com.zk.msscbeerorderservice.domain.BeerOrderEventEnum;
+import com.zk.msscbeerorderservice.domain.BeerOrderStatusEnum;
+import com.zk.msscbeerorderservice.repositories.BeerOrderRepository;
+import com.zk.msscbeerorderservice.services.BeerOrderManagerImpl;
+import com.zk.msscbeerorderservice.web.mappers.BeerOrderMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DeallocateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final JmsTemplate jmsTemplate;
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        Optional<BeerOrder> beerOrderOptional = beerOrderRepository.findById(UUID.fromString(beerOrderId));
+
+        beerOrderOptional.ifPresentOrElse(beerOrder -> {
+            jmsTemplate.convertAndSend(JmsConfig.DEALLOCATE_ORDER_QUEUE,
+                    DeallocateOrderResult.builder()
+                            .beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder))
+                            .build());
+            log.debug("Sent Deallocation Request for order id: " + beerOrderId);
+        }, () -> log.error("Beer Order Not Found!"));
+    }
+}
